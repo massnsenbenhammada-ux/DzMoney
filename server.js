a2c942cfce49ddad453c253c0d4251464fdaab0e
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-08-21 05:43:29 +0100

    feat(ui): serve Mini App without changing Squad API

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
 const { query } = require('./src/db/pool');
 const squadRoutes = require('./src/http/squad-routes');
 
 const app = express();
 const port = Number(process.env.PORT || 3000);
+const publicDir = path.join(__dirname, 'public');
 
+app.disable('x-powered-by');
 app.use(express.json());
+app.use(express.static(publicDir, { index: 'index.html' }));
 
 app.get('/health', (_req, res) => {
   res.json({ ok: true, service: 'DzMoney', version: '2.0.0' });
@@ -22,6 +26,10 @@ app.get('/health/db', async (_req, res) => {
 });
 
 app.use('/api/squad', squadRoutes);
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(publicDir, 'index.html'));
+});
 
 app.use((error, _req, res, _next) => {
   console.error('Unhandled request error:', error);
