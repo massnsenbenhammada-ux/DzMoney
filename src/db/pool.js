563eb4073410d45ade032c2fd3b42318d1a0b9de
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-08-21 19:40:02 +0100

    test: allow explicit non-SSL database connections in CI

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,8 +1,11 @@
 const { Pool } = require('pg');
 
+const hasDatabaseUrl = Boolean(process.env.DATABASE_URL);
+const sslEnabled = process.env.DATABASE_SSL === 'true' || (hasDatabaseUrl && process.env.DATABASE_SSL !== 'false');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : undefined,
+  ssl: sslEnabled ? { rejectUnauthorized: false } : undefined,
 });
 
 async function query(text, params) {
