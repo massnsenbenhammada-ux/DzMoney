5e91a36b78603686a25056c957d05ad330570ef4
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-08-21 11:33:21 +0100

    feat(ui): serve DzMoney Mini App and authenticated bootstrap

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
 const { query } = require('./src/db/pool');
 const squadRoutes = require('./src/http/squad-routes');
+const { telegramAuth } = require('./src/http/telegram-auth');
 
 const app = express();
 const port = Number(process.env.PORT || 3000);
+const publicDir = path.join(__dirname, 'public');
 
+app.disable('x-powered-by');
 app.use(express.json());
+app.use(express.static(publicDir, { index: 'index.html' }));
 
 app.get('/health', (_req, res) => {
   res.json({ ok: true, service: 'DzMoney', version: '2.0.0' });
@@ -21,7 +26,25 @@ app.get('/health/db', async (_req, res) => {
   }
 });
 
+app.get('/api/me', telegramAuth, (req, res) => {
+  const user = req.telegramUser;
+  res.json({
+    ok: true,
+    user: {
+      id: String(user.id),
+      firstName: user.first_name || '',
+      lastName: user.last_name || '',
+      username: user.username || null,
+      languageCode: user.language_code || null
+    }
+  });
+});
+
 app.use('/api/squad', squadRoutes);
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(publicDir, 'index.html'));
+});
 
 app.use((error, _req, res, _next) => {
   console.error('Unhandled request error:', error);
