06868cb0ea65f7d0a743bf89aedae3ccad1ac579
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-08-23 00:36:12 +0100

    refactor: remove unused duplicate ads provider abstraction

--- src/providers/ads/ads-provider.js
+++
@@ -1,7 +0,0 @@
-class AdsProvider {
-  async verifyCompletion() {
-    throw new Error('AdsProvider.verifyCompletion must be implemented by a provider adapter');
-  }
-}
-
-module.exports = { AdsProvider };
