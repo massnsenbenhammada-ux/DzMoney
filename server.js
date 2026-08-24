7a32c808d6203e5cd2e90e21880541d6449c696d
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-08-24 03:07:05 +0100

    feat: mount authenticated task click route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { query } = require('./src/db/pool');
 const meRoutes = require('./src/http/me-routes');
 const { createDailyCheckinRouter } = require('./src/http/daily-checkin-routes');
 const { createMonetagPostbackRouter } = require('./src/http/monetag-postback-routes');
+const { createTaskRouter } = require('./src/http/task-routes');
 const providerRegistry = require('./src/services/ad-provider-registry-runtime');
 
 const app = express();
@@ -30,6 +31,7 @@ app.get('/health/db', async (_req, res) => {
 });
 
 app.use('/api/me', meRoutes);
+app.use('/api/tasks', createTaskRouter());
 app.use('/api/daily-checkin', createDailyCheckinRouter({ providerRegistry }));
 if (monetagPostbackSecret) {
   app.use('/api/ads/monetag/postback', createMonetagPostbackRouter({
