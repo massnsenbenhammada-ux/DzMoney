7a0b0cf39f7b0f735927994367b601afd57fa853
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-08-25 14:09:10 +0100

    fix: exclude DZP from referral lifetime calculation

--- src/services/referral-activity-bridge.js
+++ src/services/referral-activity-bridge.js
@@ -1,6 +1,6 @@
 async function qualifyReferralFromActivityOnClient(client, args) {
   const { qualifyReferralFromActivityOnClient: qualify } = require('./referral-service');
-  return qualify(client, args);
+  return qualify(client, { ...args, baseReward: { coin: Number(args.baseReward?.coin || 0), dzx: Number(args.baseReward?.dzx || 0), dzp: 0 } });
 }
 
 module.exports = { qualifyReferralFromActivityOnClient };
