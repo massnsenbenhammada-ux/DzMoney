bad3037f240f4be47ac861d4ae846fa97d55a175
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-08-25 17:02:29 +0100

    feat: add immutable user referral code foundation

---
+++ migrations/018_referral_code.sql
@@ -0,0 +1,11 @@
+ALTER TABLE users ADD COLUMN referral_code TEXT;
+
+UPDATE users
+SET referral_code = UPPER(SUBSTRING(md5(id::text || ':' || random()::text || ':' || clock_timestamp()::text), 1, 10))
+WHERE referral_code IS NULL;
+
+ALTER TABLE users
+  ALTER COLUMN referral_code SET NOT NULL,
+  ADD CONSTRAINT users_referral_code_format CHECK (referral_code ~ '^[A-Z0-9]{10}$');
+
+CREATE UNIQUE INDEX idx_users_referral_code ON users(referral_code);
