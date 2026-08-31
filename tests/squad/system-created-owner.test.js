527b03793d84220087da550f9ce54f0357bc8238
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-08-31 05:51:53 +0100

    chore: remove premature Squad test

--- tests/squad/system-created-owner.test.js
+++
@@ -1,9 +0,0 @@
-import assert from 'node:assert/strict';
-import test from 'node:test';
-
-test('Squads are system-created and users cannot self-assign as owners', () => {
-  const policy = { creation: 'system', ownerAssignment: 'system' };
-
-  assert.equal(policy.creation, 'system');
-  assert.equal(policy.ownerAssignment, 'system');
-});
