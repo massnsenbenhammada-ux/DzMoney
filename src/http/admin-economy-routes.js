90132d6aa9c364ad77b2f8fb4de5ef7acc364025
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-09-07 14:26:14 +0100

    feat(phase12): add admin economy settings route

---
+++ src/http/admin-economy-routes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { adminAuth } = require('./admin-auth');
+const { getEconomySettings, setEconomySetting } = require('../services/admin-settings-service');
+
+function createAdminEconomyRouter() {
+  const router = express.Router();
+  router.use(adminAuth);
+
+  router.get('/', async (_req, res, next) => {
+    try {
+      res.json({ ok: true, settings: await getEconomySettings() });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.put('/:key', async (req, res, next) => {
+    try {
+      const result = await setEconomySetting({
+        key: req.params.key,
+        value: req.body?.value,
+        actorTelegramUserId: req.adminTelegramUserId,
+      });
+      res.json({ ok: true, setting: result });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  return router;
+}
+
+module.exports = { createAdminEconomyRouter };
