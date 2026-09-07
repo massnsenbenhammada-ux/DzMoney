a2c657cb1b41f9f3789a5826eb4508c41cb68c86
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-09-07 04:49:24 +0100

    feat(phase10): add admin promo campaign routes

---
+++ src/http/admin-promo-code-routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const promoService = require('../services/promo-code-service');
+const { adminAuth } = require('./admin-auth');
+
+function createAdminPromoCodeRouter({ promo = promoService, auth = adminAuth } = {}) {
+  const router = express.Router();
+  const asyncRoute = handler => (req, res, next) => Promise.resolve(handler(req, res, next)).catch(next);
+  router.use(auth);
+
+  router.get('/', asyncRoute(async (_req, res) => {
+    res.json({ ok: true, campaigns: await promo.listPromoCampaigns() });
+  }));
+
+  router.post('/', asyncRoute(async (req, res) => {
+    const result = await promo.createPromoCampaign(req.body || {});
+    res.status(201).json({ ok: true, campaign: result });
+  }));
+
+  router.patch('/:id', asyncRoute(async (req, res) => {
+    const result = await promo.updatePromoCampaign(Number(req.params.id), req.body || {}, req.adminTelegramUserId);
+    res.json({ ok: true, campaign: result });
+  }));
+
+  return router;
+}
+
+module.exports = { createAdminPromoCodeRouter };
