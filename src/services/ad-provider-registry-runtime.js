ee41417a6ca5afa54dba332da0e010eda1742c56
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-09-08 05:15:00 +0100

    feat(adsgram): register Test Reward provider in canonical registry

--- src/services/ad-provider-registry-runtime.js
+++ src/services/ad-provider-registry-runtime.js
@@ -3,12 +3,14 @@ const { ONCLICKA_ENABLED } = require('../config/onclicka');
 const { createMonetagProvider } = require('./monetag-adapter');
 const { createOnclickaProvider } = require('./onclicka-adapter');
 const { createGigaPubProvider } = require('./gigapub-adapter');
+const { createAdsgramProvider } = require('./adsgram-adapter');
 
-// Single runtime registry. Provider enablement comes from the canonical config.
+// Single runtime registry. Provider enablement comes from canonical config.
 const registry = new AdProviderRegistry([
   createGigaPubProvider(),
   createOnclickaProvider({ enabled: ONCLICKA_ENABLED }),
-  createMonetagProvider()
+  createMonetagProvider(),
+  createAdsgramProvider()
 ]);
 
 module.exports = registry;
