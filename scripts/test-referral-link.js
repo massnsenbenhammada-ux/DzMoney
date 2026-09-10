0a51252874bc572c19b885c02babc741c9ee7d68
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-09-10 01:26:44 +0100

    chore(odrca): format Cluster 7 referral link test

--- scripts/test-referral-link.js
+++ scripts/test-referral-link.js
@@ -5,14 +5,14 @@ const { buildReferralLink } = require('../src/config/telegram');
 function testCanonicalDirectMiniAppLink() {
   assert.strictEqual(
     buildReferralLink('ABC123'),
-    'https://t.me/DzaMoneybot/DzMoney?startapp=ABC123'
+    'https://t.me/DzaMoneybot/DzMoney?startapp=ABC123',
   );
 }
 
 function testReferralCodeIsEncoded() {
   assert.strictEqual(
     buildReferralLink('ABC+123'),
-    'https://t.me/DzaMoneybot/DzMoney?startapp=ABC%2B123'
+    'https://t.me/DzaMoneybot/DzMoney?startapp=ABC%2B123',
   );
 }
 
