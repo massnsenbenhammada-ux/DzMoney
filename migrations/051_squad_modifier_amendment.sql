ed694c87650f24f4bec7f064b09f7a6cc8ae5e69
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-09-12 20:54:46 +0100

    revert(squad): remove premature modifier migration

--- migrations/051_squad_modifier_amendment.sql
+++
@@ -1,6 +0,0 @@
--- Squad modifier amendment: uncapped rate and raw pre-modifier contribution semantics.
-ALTER TABLE squad_daily_states
-  DROP CONSTRAINT IF EXISTS squad_daily_states_modifier_rate_check;
-
-ALTER TABLE squad_daily_states
-  ADD CONSTRAINT squad_daily_states_modifier_rate_check CHECK (modifier_rate >= 0);
