ba699522de3465d6eb2192dfa0154eda1a938908
massnsenbenhammada-ux <massnsenbenhammada@gmail.com>
2026-09-15 07:21:35 +0100

    feat(squad): remove Phase 7 modifier rate cap

---
+++ migrations/054_squad_phase7_modifier_rate.sql
@@ -0,0 +1,5 @@
+ALTER TABLE squad_daily_states
+  DROP CONSTRAINT IF EXISTS squad_daily_states_modifier_rate_check;
+
+ALTER TABLE squad_daily_states
+  ADD CONSTRAINT squad_daily_states_modifier_rate_check CHECK (modifier_rate >= 0);
